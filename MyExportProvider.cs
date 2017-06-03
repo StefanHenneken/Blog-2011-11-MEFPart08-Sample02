71c3880c3c639e155280cf75fd5a021a06e8b37c
Stefan Henneken <stefan.henneken@live.com>
2017-06-03 23:31:52 +0200

    Add project files.

---
+++ MyExportProvider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+using System.Linq;
+using System.Reflection;
+
+namespace Sample02
+{
+    public class MyExportProvider : ExportProvider
+    {
+        private List<Export> Exports { get; set; }
+        private string TypeName { get; set; }
+
+        public MyExportProvider(string contractName, string typeName)
+        {
+            Func<object> funcCreatePart = new Func<object>(CreatePart);
+            this.TypeName = typeName;
+
+            this.Exports = new List<Export>();
+            var metadata = new Dictionary<string, object>();
+            metadata.Add(CompositionConstants.ExportTypeIdentityMetadataName, contractName);
+
+            var exportDefinition = new ExportDefinition(contractName, metadata);
+            var export = new Export(exportDefinition, funcCreatePart);
+            this.Exports.Add(export);
+        }
+
+        public object CreatePart()
+        {
+            Type partType = Assembly.GetExecutingAssembly().GetType(this.TypeName);
+            object instance = Activator.CreateInstance(partType); 
+            return instance;
+        }
+
+        protected override IEnumerable<Export> GetExportsCore(ImportDefinition definition, AtomicComposition atomicComposition)
+        {
+            return this.Exports.Where(x => definition.IsConstraintSatisfiedBy(x.Definition));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Sample02
+{     
+    public class Program
+    {
+        [ImportMany]
+        private ICarContract[] CarParts { get; set; }
+      
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+
+        void Run()
+        {
+            CompositionContainer container = null;
+
+            try
+            {
+                var catalog = new AssemblyCatalog(Assembly.GetExecutingAssembly());
+                var provider = new MyExportProvider(typeof(ICarContract).FullName, typeof(BMW).FullName);
+                container = new CompositionContainer(catalog, provider);
+                container.ComposeParts(this);
+
+                foreach (ICarContract carPart in CarParts)
+                    Console.WriteLine(carPart.GetName());
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            container.Dispose();
+        }
+    }
+
+    public interface ICarContract
+    {
+        string GetName();
+    }
+
+    [Export(typeof(ICarContract))]
+    public class Mercedes : ICarContract
+    {
+        public string GetName()
+        {
+            return "Mercedes";
+        }
+    }
+    
+    public class BMW : ICarContract
+    {
+        public string GetName()
+        {
+            return "BMW";
+        }
+    }
+}
